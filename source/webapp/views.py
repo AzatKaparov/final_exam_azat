afaa8ae3d73f83948839c61ac9a6ea35c4673ab1
azat <azatkaparov843@gmail.com>
2021-01-23 11:35:43 +0600

    Сделана IndexView

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from urllib import request
 
-# Create your views here.
+from django.shortcuts import render
+from django.views.generic import ListView
+from django.contrib.auth.models import User
+
+
+class IndexView(ListView):
+    template_name = 'index.html'
+    context_object_name = 'friends'
+    paginate_by = 10
+    paginate_orphans = 0
+    model = User
+    ordering = ['username']
+
+    def get_queryset(self):
+        data = super().get_queryset()
+        # if not self.request.GET.get('is_admin', None):
+        #     data = data.filter(status='moderated')
+        return data
